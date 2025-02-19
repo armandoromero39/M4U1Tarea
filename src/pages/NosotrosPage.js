d5ad215413dcf61a6f8da734097af8e51d4309de
Armando Romero <arman2314@gmail.com>
2025-02-19 09:56:49 -0300

    arreglado los alt en la pagina de nosotros

--- src/pages/NosotrosPage.js
+++ src/pages/NosotrosPage.js
@@ -10,21 +10,21 @@ const NosotrosPage = (props) => {
             <h2>Staff</h2>
             <div className="personas">
                 <div className="persona">
-                    <img src="img/nosotros/dueño.jpg" />
+                    <img src="img/nosotros/dueño.jpg" alt="Armando Romero" />
                     <h5>Armando Romero</h5>
                     <h6>Dueño</h6>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 </div>
                 <div className="persona">
-                    <img src="img/nosotros/dueña.jpg" />
+                    <img src="img/nosotros/dueña.jpg" alt="Nathalia Ojeda" />
                     <h5>Nathalia Ojeda</h5>
                     <h6>Dueña</h6>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 </div>
                 <div className="persona">
-                    <img src="img/nosotros/vendedora.jpg" />
-                    <h5>Vendedora</h5>
-                    <h6>Gerente de Sistemas</h6>
+                    <img src="img/nosotros/vendedora.jpg" alt="Valentina Milagro" />
+                    <h5>Valentina Milagro</h5>
+                    <h6>Vendedora</h6>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 </div>
           
