94297c9a45477b524e30b1852d794e13683ab894
Armando Romero <arman2314@gmail.com>
2025-05-01 16:42:26 -0300

    ultimos cambios para la api del back

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,13 +1,34 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/novedades/NovedadItem';
+
 const NovedadesPage = (props) => {
+    
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(()=>{
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+
+        cargarNovedades();
+    }, []);
+    
+    
+    
     return(
         <section className="holder">
-        <h2>Novedades</h2>
-        <div className="novedades">
-            <h1>Título</h1>
-            <h2>Subtítulo</h2>
-            <p>Cuerpo</p>
-            <hr/>
-        </div>
+         {loading ? (
+            <p>Cargando...</p>
+        ) : (
+                novedades.map(item => <NovedadItem key={item.id}
+                    title = {item.titulo} subtitle = {item.subtitulo}
+                    imagen = {item.imagen} body = {item.cuerpo} />)
+        )}
        </section>
     )
 }
